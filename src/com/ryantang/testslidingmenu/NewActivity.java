784df219f74c1ac84ff1cb6f845936213c70bafe
RyanTang <ryantang1988@gmail.com>
2013-05-09 10:49:55 +0800

    v1.0

--- src/com/ryantang/testslidingmenu/NewActivity.java
+++ src/com/ryantang/testslidingmenu/NewActivity.java
@@ -1,8 +1,7 @@
 package com.ryantang.testslidingmenu;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
 import android.widget.TextView;
 
 public class NewActivity extends Activity {
@@ -13,13 +12,6 @@ public class NewActivity extends Activity {
 		setContentView(R.layout.main_page_layout);
 		TextView textView = (TextView)this.findViewById(R.id.tv_text);
 		textView.setText("This is a new activity!");
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
 	}
 
 }
